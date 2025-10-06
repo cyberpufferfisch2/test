eb9ef70d9adc0675fea1edb3ef6f6115701b8790
cyberpufferfisch2 <annkatrin.reeh@gmx.de>
2025-10-06 12:18:08 +0200

    python

---
+++ einführung informatik/bonusaufgabe.py
@@ -0,0 +1,9 @@
+list = [7, 3, 10, 0, 5, 2, 8, 1, 4, 6, 9, 3]
+
+def zahl_finden (list,zahl):
+    if zahl in list:
+        print(list.index(zahl))
+    else:
+        print("Ist nicht da lol")      
+   
+zahl_finden(list,14)
